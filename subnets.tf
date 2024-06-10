19d689299644faa1b800b3942b8ccd9585fe3332
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-06-10 15:36:55 -0400

    auto push

--- subnets.tf
+++ subnets.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   count = length(var.PRIVATE_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
 
   tags = {
     Name = "roboshop-${var.ENV}-private-subnet-${count.index}"
