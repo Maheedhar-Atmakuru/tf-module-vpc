da7516b990c99a56b50dcfa1f8f4a9c78bd57997
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-06-10 18:58:44 -0400

    auto push

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 #Creates peering connection
 
-resource "aws_vpc_peering_connection" "peering" {
+resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_CIDR
   auto_accept   = true
