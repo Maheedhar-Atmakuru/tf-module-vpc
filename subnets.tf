f9a600c5eec4ef447013c5752ee697e3c26f96c0
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-06-10 15:12:21 -0400

    auto push

---
+++ subnets.tf
@@ -0,0 +1,21 @@
+# Creates public subnets
+resource "aws_subnet" "public_subnet" {
+  count = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-subnet-${count.index}"
+  }
+}
+
+# Creates private subnets
+resource "aws_subnet" "public_subnet" {
+  count = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-subnet-${count.index}"
+  }
+}
