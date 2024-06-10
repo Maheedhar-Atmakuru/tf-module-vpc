a59f861aa40f8cb04f5fa81e748327eea5b02cf2
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-06-10 18:00:38 -0400

    auto push

--- routetable.tf
+++ routetable.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "public_rt" {
 
 resource "aws_route_table_association" "public_subnet_rt_association" {
     count = length(aws_subnet.public_subnet.*.id)
-  subnet_id      = element(aws_subnet.public_subnet.*.id,count_index)
+  subnet_id      = element(aws_subnet.public_subnet.*.id,count.index)
   route_table_id = aws_route_table.public_rt.id
 }
 
@@ -36,6 +36,6 @@ resource "aws_route_table" "private_rt" {
 
 resource "aws_route_table_association" "private_subnet_rt_association" {
     count = length(aws_subnet.private_subnet.*.id)
-  subnet_id      = element(aws_subnet.private_subnet.*.id,count_index)
+  subnet_id      = element(aws_subnet.private_subnet.*.id,count.index)
   route_table_id = aws_route_table.private_rt.id
 }
