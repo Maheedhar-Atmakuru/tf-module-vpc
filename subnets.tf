6df4d8045e39ef950c66b1c749e16b512e614b37
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-06-10 15:17:06 -0400

    auto push

--- subnets.tf
+++ subnets.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 # Creates private subnets
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "private_subnet" {
   count = length(var.PRIVATE_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
