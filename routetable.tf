f029ce02b869f31a12850378bdd435ea62f52ba8
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-06-10 16:14:43 -0400

    auto push

--- routetable.tf
+++ routetable.tf
@@ -1,5 +1,5 @@
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "10.0.1.0/24"
@@ -12,8 +12,8 @@ resource "aws_route_table" "example" {
   }
 }
 
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
 
 #   route {
 #     cidr_block = var.VPC_CIDR
